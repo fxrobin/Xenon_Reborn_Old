73ba4d1f9db360e7d0a511491f3549a5555918f6
fxrobin <fxrobin@gmail.com>
2017-11-18 00:53:44 +0100

    pour compléter le commit précédent.

---
+++ RBN_XenonReborn/src/net/entetrs/xenon/screens/XenonScreen.java
@@ -0,0 +1,29 @@
+package net.entetrs.xenon.screens;
+
+import java.util.function.Supplier;
+
+import com.badlogic.gdx.Screen;
+
+/**
+ * enum complexe avec lambda pour la création des écrans
+ * associé à leur constructeur.
+ * 
+ * @author robin
+ *
+ */
+public enum XenonScreen
+{
+	MENU(MenuScreen::new), GAME_PLAY(GamePlayScreen::new);
+
+	private Supplier<Screen> supplier;
+
+	private XenonScreen(Supplier<Screen> supplier)
+	{
+		this.supplier = supplier;
+	}
+
+	public Screen createScreen()
+	{
+		return supplier.get();
+	}
+}

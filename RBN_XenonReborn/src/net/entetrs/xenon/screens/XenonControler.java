48685c77da13560170a684ec9ca554cabb1eb733
fxrobin <fxrobin@gmail.com>
2017-12-03 23:02:16 +0100

    ajout du controler

---
+++ RBN_XenonReborn/src/net/entetrs/xenon/screens/XenonControler.java
@@ -0,0 +1,17 @@
+package net.entetrs.xenon.screens;
+
+/**
+ * tout contrôleur doit pouvoir afficher un écran.
+ * 
+ * @author robin
+ *
+ */
+public interface XenonControler
+{
+	/**
+	 * affiche un écran.
+	 * 
+	 * @param screen
+	 */
+	void showScreen(XenonScreen screen);
+}

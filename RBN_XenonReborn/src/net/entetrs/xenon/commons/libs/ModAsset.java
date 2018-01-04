4375693e5dd16f833fc59ca1fbd6374ee0b37ced
fxrobin <fxrobin@gmail.com>
2018-01-04 06:37:36 +0100

    ajout de modules sons.

--- RBN_XenonReborn/src/net/entetrs/xenon/commons/libs/ModAsset.java
+++ RBN_XenonReborn/src/net/entetrs/xenon/commons/libs/ModAsset.java
@@ -2,10 +2,14 @@ package net.entetrs.xenon.commons.libs;
 
 public enum ModAsset
 {
+	/* temporaire : utiliser implémentation XM : XENON_REMIX("mods/aa_xenon.xm") */
 	FAKTORY("mods/faktory.mod"),
 	DOODLE_DOO("mods/doodle-doo.mod"),
 	TRANCE_124("mods/trance-124.mod"),
-	BREATH_TAKER("mods/breathtaker.mod");
+	BREATH_TAKER("mods/breathtaker.mod"),
+	XENON_ORIGINAL("mods/xenon2.mod"),
+	/* temporaire : utiliser implémentation XM : XENON_200("mods/xenon220.xm") */
+	;
 
 	final String modName;
 

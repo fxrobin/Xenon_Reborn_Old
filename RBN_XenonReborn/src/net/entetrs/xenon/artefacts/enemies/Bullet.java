92200147bcd228d7ee8cba44d3bca319a41ed5fb
fxrobin <fxrobin@gmail.com>
2018-01-07 17:56:34 +0100

    un peu de javadoc.

--- RBN_XenonReborn/src/net/entetrs/xenon/artefacts/enemies/Bullet.java
+++ RBN_XenonReborn/src/net/entetrs/xenon/artefacts/enemies/Bullet.java
@@ -4,6 +4,14 @@ import net.entetrs.xenon.artefacts.Artefact;
 import net.entetrs.xenon.artefacts.friendly.Ship;
 import net.entetrs.xenon.commons.libs.TextureAsset;
 
+/**
+ * représente un "bullet", un tir de l'ennemi. Seule les collisions avec le
+ * vaisseau du joueur sont prises en compte, c'est à dire qu'une bullet n'entre
+ * pas en collision avec les tirs du vaisseau.
+ * 
+ * @author robin
+ *
+ */
 public class Bullet extends Enemy
 {
 	public Bullet()
@@ -11,10 +19,15 @@ public class Bullet extends Enemy
 		super(TextureAsset.BULLET.get(), 5, 5, 8);
 	}
 
+	/*
+	 * redéfinition de la méthode "isCollision" pour ne rentrer en coolision
+	 * qu'avec le vaisseau.
+	 */
+
 	@Override
 	public boolean isCollision(Artefact otherArtefact)
 	{
-        /* collision uniquement avec le vaisseau */
+		/* une petite ternaire pour la route ... */
 		return otherArtefact instanceof Ship ? super.isCollision(otherArtefact) : false;
 	}
 }

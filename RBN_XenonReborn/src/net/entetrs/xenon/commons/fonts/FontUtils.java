662b74e980b337225b45181d9d4189727e4c77bb
FX Robin <fxrobin@gmail.com>
2018-04-10 00:22:59 +0200

    re"fonte" de la partie "bitmapfont".
    Ce n'est pas totalement fini, mais c'est déjà une petite avancée.

--- RBN_XenonReborn/src/net/entetrs/xenon/commons/fonts/FontUtils.java
+++ RBN_XenonReborn/src/net/entetrs/xenon/commons/fonts/FontUtils.java
@@ -12,41 +12,34 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 import net.entetrs.xenon.commons.libs.TextureAsset;
 
-public final class FontUtils
-{
+public final class FontUtils {
 	private static final Log log = LogFactory.getLog(FontUtils.class);
+	private static BitmapFont bitmapFont;
+	private static int fontWidth;
+	private static int fontHeight;
+	private static String stringMap;
 
-	static
-	{
+	static {
 		Properties config = new Properties();
-		try
-		{
+		try {
+			Texture font = TextureAsset.FONT.get();
 			config.load(FontUtils.class.getResourceAsStream("/fonts/font-blue.properties"));
 			fontWidth = Integer.parseInt(config.getProperty("letter-width"));
 			fontHeight = Integer.parseInt(config.getProperty("letter-height"));
 			stringMap = config.getProperty("string-map");
+			bitmapFont = BitmapFont.build(font, fontWidth, fontHeight, stringMap);
 			log.info("Font loaded");
-		}
-		catch (IOException e)
-		{
+		} catch (IOException e) {
 			log.error("Impossible de lire le fichier de ressource de font.");
 		}
 
 	}
 
-	private static int fontWidth;
-	private static int fontHeight;
-	private static String stringMap;
-
-	private static Texture font = TextureAsset.FONT.get();
-
-	private FontUtils()
-	{
+	private FontUtils() {
 		/* protection */
 	}
 
-	public static int getWidth(String txt)
-	{
+	public static int getWidth(String txt) {
 		return txt.length() * fontWidth;
 	}
 
@@ -55,43 +48,28 @@ public final class FontUtils
 		return fontHeight;
 	}
 
-	public static int getFontWidth()
-	{
+	public static int getFontWidth() {
 		return fontWidth;
 	}
 
-	public static void setFontHeight(int fontHeight)
-	{
+	public static void setFontHeight(int fontHeight) {
 		FontUtils.fontHeight = fontHeight;
 	}
 
-	public static void print(Batch b, float x, float y, String txt)
-	{
-		for (int i = 0; i < txt.length(); i++)
-		{
+	public static void print(Batch b, float x, float y, String txt) {
+		for (int i = 0; i < txt.length(); i++) {
 			print(b, x + (i * fontWidth), y, txt.charAt(i));
 		}
 	}
 
-	public static void print(Batch batch, float positionX, float positionY, char character)
-	{
-		int offset = getCharOffset(character);
-		if (offset >= 0)
-		{
-			batch.draw(font, positionX, positionY, offset, 0, fontWidth, fontHeight);
-		}
+	public static void print(Batch batch, float positionX, float positionY, char character) {
+		TextureRegion tr = bitmapFont.getChar(character);
+		if (tr != null) batch.draw(tr, positionX, positionY);
 	}
 
-	public static int getCharOffset(char character)
-	{
-		int charIndex = stringMap.indexOf(character);
-		return (charIndex >= 0) ? charIndex * fontWidth : -1;
-	}
 
-	public static TextureRegion getTextureRegionOfChar(char character)
-	{
-		int offset = getCharOffset(character);
-		return (offset >= 0) ? new TextureRegion(font, offset, 0, fontWidth, fontHeight) : null;
+	public static TextureRegion getTextureRegionOfChar(char character) {
+		return bitmapFont.getChar(character);
 	}
 
 }

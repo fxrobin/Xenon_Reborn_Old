0324ba7c0edea261244c7d40d2f96fd50eb71656
F.X. Robin <fxrobin@gmail.com>
2018-07-02 14:36:31 +0200

    PB GIT résolu.

---
+++ RBN_XenonReborn/core/src/net/entetrs/xenon/artefacts/Event.java
@@ -0,0 +1,13 @@
+package net.entetrs.xenon.artefacts;
+
+/**
+ * évenements déclenchés pendant la durée de vie d'un artefact (Observer / Observable pattern).
+ * 
+ * 
+ * @author robin
+ *
+ */
+public enum Event 
+{
+	CREATED, HIT, DESTROYED;
+}
